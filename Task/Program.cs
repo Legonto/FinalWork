7b89d69c8604f38db8c9ca8f1dc0eb0d92c38ef7
Elena Gontovaya <lena_tuman@mail.ru>
2022-09-21 12:21:15 +0300

    FW_newArr

---
+++ Task/Program.cs
@@ -0,0 +1,3 @@
+﻿string[] arr  = new string[] {"Hello", "23", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan"};
+Console.WriteLine("Наш изначальный массив:");
+Console.WriteLine($"[{String.Join("; ", arr)}]");
